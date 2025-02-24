14b7f302784f3cbe240dca48e719f2493e9940da
Shalini <bodeshalinireddy@gmail.com>
2025-02-24 12:00:00 -0600

    added svd algorithm

--- src/recommendation_system.py
+++ src/recommendation_system.py
@@ -1,4 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD
 
 def compute_cosine_similarity(user_movie_matrix):
     """Compute cosine similarity between movies."""
@@ -10,3 +11,9 @@ def recommend_movies(movie_name, cosine_sim_df, top_n=5):
     similar_movies = similar_scores.sort_values(ascending=False)
     similar_movies = similar_movies.drop(movie_name)
     return similar_movies.head(top_n)
+
+def svd_recommendation(user_movie_matrix, num_components=20):
+    svd = TruncatedSVD(n_components=num_components)
+    matrix_svd = svd.fit_transform(user_movie_matrix)
+    cosine_sim = cosine_similarity(matrix_svd)
+    return cosine_sim
